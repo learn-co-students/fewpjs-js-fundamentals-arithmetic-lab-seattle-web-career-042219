a665e56900d439eb8265174696a1167f6e1f3c8d
austenhasty <hastyae@gmail.com>
2019-06-03 12:55:08 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = (num1 * num2);
+
+let random = Math.random() * 10;
+
+let mod = 59 % 5
+
+let max = Math.max(1, 5, 13, 20)
